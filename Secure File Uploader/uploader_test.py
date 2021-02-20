1b63deb7709439c6d53e8a4336afe6c04de3cb1d
primnp <51148602+primnp@users.noreply.github.com>
2021-02-20 18:08:01 -0500

    Update uploader_test.py

--- Secure File Uploader/uploader_test.py
+++ Secure File Uploader/uploader_test.py
@@ -1,4 +1,4 @@
 from uploader import *
 
 def test_uploader():
-  assert uploader({"name":"Sarah", "age": 24, "isEmployed": True }) == {"name": "Sarah", "age": 24, "isEmployed": true}
+  assert uploader({"name":"Sarah", "age": 24, "isEmployed": true }) == {"name": "Sarah", "age": 24, "isEmployed": true}
