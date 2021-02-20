a281de8581f4e647fd9db056e5e434564fb73371
primnp <51148602+primnp@users.noreply.github.com>
2021-02-20 18:13:42 -0500

    Update uploader_test.py

--- Secure File Uploader/uploader_test.py
+++ Secure File Uploader/uploader_test.py
@@ -1,4 +1,4 @@
 from uploader import *
 
 def test_uploader():
-  assert uploader({"name":"My_username", "ID": 24, "isActive": True }) == {"name": "My_username", "ID": 24, "isActive": True}
+  assert uploader() == "test create file"
