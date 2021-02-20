8a1a33b4ebc1cebe14c2ab1a85bb079c3c51593b
primnp <51148602+primnp@users.noreply.github.com>
2021-02-20 18:13:27 -0500

    Update uploader.py

--- Secure File Uploader/uploader.py
+++ Secure File Uploader/uploader.py
@@ -1,6 +1,17 @@
 import json
 
 def uploader(x):
-    py2Json = json.dumps(x)
-    return py2Json
+   #input file
+   #pseudo implementation
+   # file.time_stamp = event(upload, timestamp)    
+   # if filename == NULL
+   #     return "Error filename"
+   # file.filename = filename
+   # file.userID = 20
+   # file.permissions = 20, 21
+   # file.id = "/file/011"
+   # file.text = "a very long text"
+   # file.fileURL = "https://example.test/file/011/20"
+   # ...
+    return "test create file"
 
