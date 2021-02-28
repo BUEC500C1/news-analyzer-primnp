9dccdd6e83b4fe7b0cab08420f11736da6cec236
Prim P <p.nuwapa@gmail.com>
2021-02-28 13:11:46 -0500

    update restful uploader

--- News Feed Ingester/newsingester.py
+++ News Feed Ingester/newsingester.py
@@ -1,3 +1,5 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 #create
 def ingestFile():
--- Secure File Uploader/uploader.py
+++ Secure File Uploader/uploader.py
@@ -1,9 +1,47 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
 import json
 
-def uploader():
+app = Flask(__name__)
+api = Api(app)
+
+#just testing functionalities, not real implementations
+
+class FileUploader(Resource):
+    def get(self):
+        return {'about': 'test create file'}
+        # return FILES
+
+    def post(self):
+        json_file = request.get_json()
+        return {'file sent': json_file}, 201
+
+    #def delete
+
+class EditFile(Resource):
+    def get(self, file_id):
+        return {'about': file_id}
+
+    def put(self): #def put(self, file_id):
+        edit_json = request.get_json()
+        return {'edited': edit_json}, 201
+
+class User(Resource):
+    def get(self):
+        return {'about': 'test user'}
+
+    def post(self):
+        json_user = request.get_json()
+        return {'user info sent': json_user}, 201
+
+    #def PUT
+    #def delete
+
+
+# def uploader():
    #input file
    #pseudo implementation
-   # file.time_stamp = event(upload, timestamp)    
+   # file.time_stamp = event(upload, timestamp)
    # if filename == NULL
    #     return "Error filename"
    # file.filename = filename
@@ -13,6 +51,15 @@ def uploader():
    # file.text = "a very long text"
    # file.fileURL = "https://example.test/file/011/20"
    # ...
-   return "test create file"
-   
+# return "test create file"
 
+@app.route("/")
+def index():
+    return "Secure File Uploader API with RESTFUL"
+
+api.add_resource(FileUploader, '/file')
+api.add_resource(EditFile, '/file/File_Info/<int:file_id>')
+api.add_resource(User, '/user')
+
+if __name__ == '__main__':
+    app.run(debug=True)
