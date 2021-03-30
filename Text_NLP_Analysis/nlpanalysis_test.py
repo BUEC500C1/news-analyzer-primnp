bf7eaf0183cdad3cfc13247c33d6d71e68004eba
primnp <51148602+primnp@users.noreply.github.com>
2021-03-29 20:12:54 -0400

    Update nlpanalysis_test.py

--- Text_NLP_Analysis/nlpanalysis_test.py
+++ Text_NLP_Analysis/nlpanalysis_test.py
@@ -2,24 +2,49 @@ from nlpanalysis import *
 import nlpanalysis_calc as nlp
 
 def test_sentiment():
-    assert searchOnKeywords() == "list of files with keywords"
-    assert searchRepeatedWords() == "list of repeated words & their frequency"
-    assert searchCommonKeywords() == "the most common keyword(s)"
-    assert linkDocViaKeywords() == "possibly related documents"
-    assert findContentSentiment() == "sentiment"
-    assert linkKeywordsandSentiment() == "relationship between keywords & sentiment in numbers"
-    assert linkPreviousSearch() == "search recommendations"
-    assert translateText() == "translated text"
-    assert findBiasOnKeywords() == "bias"
+    input1 = "a quick brown fox"
+    res = nlp.returnSentiment(input1)
+    assert type(res) == float
+    
+    input2 = "John is very angry at that blue cat"
+    res2 = nlp.returnSentiment(input2)
+    assert type(res2) == float
+    
+    input3 = []
+    res3 = nlp.returnSentiment(input3)
+    assert res3 == []
+  
+def test_entity():
+    input1 = "Bitcoin is unpredictable"
+    res = nlp.returnEntities(input)
+    assert type(res) == list
+    
+    input2 = "river flows in you"
+    res2 = nlp.returnEntities(input2)
+    assert type(res2) == list
+    assert type(res2[0]) == str
+    
+    input3 = None
+    res3 = nlp.returnEntities(input3)
+    assert res3 == []
 
-def test_read():
-    assert searchBoolean() == "boolean function"
-    assert GetSearchHistory() == "(keywords, sentiments)"
-    assert ClassifyDatas() == "data format: xx"
+def test_entsent():
+    input1 = "a quick brown fox jumps over a lazy dog"
+    res = nlp.returnEntitiesAndSentiments(input1)
+    assert type(res) == list
+    assert type(res[0]) == dict
+    
+    input2 = ['1','2','3']
+    res2 = nlp.returnEntitiesAndSentiments(input2)
+    assert res2 == []
 
-def test_update():
-    assert UpdateBias() == "bias updated"
-    assert UpdateLanguage() == "language updated"
-
-def test_delete():
-    assert RemoveBiasData() == "Bias removed"
+def test_conclass():
+    input1 = "A quick brown fox jumps over a lazy dog. The lazy dog then disappear mysteriously. People start to wonder if a quick brown fox is a witch."
+    res = nlp.classifyContent(input1)
+    assert type(res) == list
+    assert type(res[0]) == dict
+    
+    input2 = "A quick brown fox jumps over a lazy dog."
+    res2 = nlp.classifyContent(input2)
+    assert res2 == null
+    
