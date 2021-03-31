597b2746a2fb194d7df79d6e946b0800de0126c8
primnp <51148602+primnp@users.noreply.github.com>
2021-03-31 16:57:22 -0400

    Update uploader_test.py

--- Secure_File_Uploader/test/uploader_test.py
+++ Secure_File_Uploader/test/uploader_test.py
@@ -5,7 +5,6 @@ def test_uploader():
   
   assert allowed_file('test.pdf') == True
   assert allowed_file('test.jpg') == False
-  assert type(retrieve_filejson('test')) != list
   assert delete_many('files') == result
   assert delete_all() == result
   assert delete_extractedmany('random') == result
