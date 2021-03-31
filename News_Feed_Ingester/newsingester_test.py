25b2c1d91eca4e5526d99527b4b9e2fa6fc0dd71
primnp <51148602+primnp@users.noreply.github.com>
2021-03-31 16:36:18 -0400

    Update newsingester_test.py

--- News_Feed_Ingester/newsingester_test.py
+++ News_Feed_Ingester/newsingester_test.py
@@ -17,19 +17,15 @@ def test_listsquery():
     input1 = ['nyc', 'apple', 'times']
     res1 = news.findWithKeywordLists(input1)
     assert type(res1[0]) == dict
-
-    input2 = 0
-    res2 = news.findWithKeywordLists(input2)
-    assert res2 == []
     
     input3 = []
     res3 = news.findWithKeywordLists(input3)
     assert res3 == []
 
 def test_dmyquery():
-    test1 = news.dmy_query('2021', '2021', '03', '03', '25', '15', ['grape', 'apple'])
+    test1 = news.findWithDMY('2021', '2021', '03', '03', '25', '15', ['grape', 'apple'])
     assert type(test1) == list
     assert type(test1[0]) == dict
     
-    test2 = news.dmy_query('2021', '2021', '03', '03', '15', '25', ['grape', 'apple'])
+    test2 = news.findWithDMY('2021', '2021', '03', '03', '15', '25', ['grape', 'apple'])
     assert test2 == []
